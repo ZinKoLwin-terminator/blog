b16b6868bebfcd9ab8bdd58b9b6adbdc400282c0
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-11 18:00:20 +0630

    collection part 1

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta
@@ -19,7 +18,8 @@
 </head>
 
 <body>
-    <?= $blog; ?>
+    <h1><?= $blog->title; ?></h1>
+    <p><?= $blog->body; ?></p>
     <a href="/">go back</a>
 </body>
 
