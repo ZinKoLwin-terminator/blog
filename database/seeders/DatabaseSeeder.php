3b2def226e8b1fd1b667c16f2237d24ced30da69
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-12 23:57:11 +0630

    what the heck is db seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Blog;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create();
 
+        $frontend = Category::create([
+            'name' => 'frontend',
+            'slug' => 'frontend',
+        ]);
+
+        $backend =  Category::create([
+            'name' => 'backend',
+            'slug' => 'backend',
+        ]);
+
+        Blog::create([
+            'title' => 'frontend',
+            'slug' => 'frontend-post',
+            'intro' => 'this is intro',
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'category_id' => $frontend->id
+        ]);
+
+
+        Blog::create([
+            'title' => 'backend',
+            'slug' => 'backend-post',
+            'intro' => 'this is intro',
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'category_id' => $backend->id
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
