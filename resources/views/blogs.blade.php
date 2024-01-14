528f55c6757b0ce3fcf654f72eb20da7f4329665
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-14 22:00:29 +0630

    make blogs dynamic

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,27 +1,11 @@
-@extends('layout')
 
-@section('title')
-<title>All blogs</title>
-@endsection
-@section('content')
-@foreach($blogs as $blog)
-<div>
-    <h1><a href="blogs/{{$blog->slug}}">
-            {{$blog->title}}
-        </a></h1>
-        <h4>Author - <a href="/users/{{$blog->author->username}}">{{$blog->author->name}}</a></h4>
-        <p>
-            <a href="/categories/{{$blog->category->slug}}">{{$blog->category->name}}</a>
-        </p>
-    <div>
-        <p>
-            published at -
-            {{$blog->created_at->diffForHumans()}}
-        </p>
-        <p>
-            {{$blog->intro}}
-        </p>
-    </div>
-</div>
-@endforeach
-@endsection
+<x-layout>
+
+<x-hero/>
+
+<x-blog-section :blogs="$blogs" />
+
+<x-subscribe/>
+
+</x-layout>
+
--- resources/views/navbar.blade.php
+++
@@ -1,5 +0,0 @@
-<nav>
-    <li><a href="">Main</a></li>
-    <li><a href="">about</a></li>
-    <li><a href="">contact</a></li>
-</nav>
