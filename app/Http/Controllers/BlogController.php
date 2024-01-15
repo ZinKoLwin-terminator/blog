e4dac68471ecffa06ae76812f1b141df997d8942
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-15 21:48:06 +0630

    introduction to controller and simple query refactoring

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    function index()
+    {
+
+        return view('blogs', [
+            'blogs' => $this->getBlogs(),
+            'categories' => Category::all(),
+
+        ]);
+    }
+
+    function  show(Blog $blog)
+    {
+        return view('blog', [
+            'blog' => $blog,
+            'randomBlogs' => Blog::inRandomOrder()->take(3)->get()
+        ]);
+    }
+
+    protected function getBlogs()
+    {
+        $blogs = Blog::latest();
+        if (request('search')) {
+            $blogs = $blogs->where('title', 'LIKE', '%' . request('search') . '%')
+                ->orWhere('body', 'LIKE', '%' . request('search') . '%');
+        }
+        return $blogs->get();
+    }
+}
