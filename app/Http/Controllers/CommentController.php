abfefc71136bb0275c10b0519a7afb13e6ea0339
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-19 16:45:56 +0630

    introduction to mail system

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SubscriberMail;
 use App\Models\Blog;
 
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -14,10 +17,18 @@ class CommentController extends Controller
             'body' => 'required | min:10'
         ]);
 
+
         $blog->comments()->create([
             'body' => request('body'),
             'user_id' => auth()->id()
         ]);
+        //mail
+
+        $subscribers = $blog->subscribers->filter(fn ($subscriber) => $subscriber->id !== auth()->id());
+
+        $subscribers->each(function ($subscriber) use ($blog) {
+            Mail::to($subscriber->email)->send(new SubscriberMail($blog));
+        });
 
         return redirect('/blogs/' . $blog->slug);
     }
